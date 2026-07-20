71d310fd5a31db0bf71358b855096523339a4e8c
Ali XSteps <sm9009995710@gmail.com>
2026-07-20 10:21:41 +0530

    chore: Temporarily remove spring security

--- backend/src/main/java/com/enterprise/knowledgehub/KnowledgehubApplication.java
+++ backend/src/main/java/com/enterprise/knowledgehub/KnowledgehubApplication.java
@@ -2,9 +2,7 @@ package com.enterprise.knowledgehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class KnowledgehubApplication {
 
 	public static void main(String[] args) {
